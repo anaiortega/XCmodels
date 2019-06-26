48dbe6fefa99e3105e3d47781756f0ca91673ddc
Ana <ana.ortega.ort@gmail.com>
2019-06-26 23:44:37 +0200

    update models

--- OXapp/prueba/env_config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from postprocess.config import default_config
-
-#home= '/home/ana/projects/XCmodels/'
-home= '/home/ana/projects/XCmodels/OXapp/'
-
-# Default configuration of environment variables.
-cfg=default_config.envConfig(language='en',intForcPath= home + 'prueba/results/internalForces/',verifPath= home + 'prueba/results/verifications/',annexPath= home + 'prueba/annex/',grWidth='120mm')
