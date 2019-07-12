cf10fb2fe83a4727d10486418b90c03157170bd2
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-07-12 18:55:47 +0200

    Sheathing design

--- OXapp/embedded_beams/sheathing/xc_model.py
+++ OXapp/embedded_beams/sheathing/xc_model.py
@@ -1,23 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import math
-import vtk
 import xc_base
 import geom
 import xc
 from solution import predefined_solutions
-from model import predefined_spaces
-from materials import typical_materials
-from materials.ec3 import EC3_materials
-
-from materials.sections import structural_steel as steel
-from actions import load_cases as lcm
-from actions import combinations as combs
-
 
 # Problem type
-mainBeam= xc.FEProblem()
-mainBeam.title= 'Warehouse main beams'
-preprocessor= mainBeam.getPreprocessor   
-nodos= preprocessor.getNodeHandler
+sheathinginBeam= xc.FEProblem()
+sheatingBeam.title= 'Sheating design'
+preprocessor= sheatingBeam.getPreprocessor   
+nodes= preprocessor.getNodeHandler
 modelSpace= predefined_spaces.StructuralMechanics2D(nodos)
