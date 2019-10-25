51179694a86a4102e641a074af2123094ac9597a
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-10-25 14:10:17 +0200

    Compute reaction of 2nd floor trusses over ramp wall

--- OXapp/wood_structure/trusses/2nd_floor_truss_F.py
+++ OXapp/wood_structure/trusses/2nd_floor_truss_F.py
@@ -13,14 +13,13 @@ from materials.sections import section_properties as sp
 from solution import predefined_solutions
 from actions import load_cases
 from actions import combinations as combs
+from postprocess.xcVtk import vtk_graphic_base
 
 inchToMeter= 2.54/100.0
 feetToMeter= 0.3048
 psfTokNm2= 0.04788026
 psfToNm2= 1e3*psfTokNm2
 centerSpacingTrusses= 24.0*inchToMeter
-centerSpacingJoists= 32.0*inchToMeter
-joistLoadFactor= centerSpacingJoists/centerSpacingTrusses
 depth= (22-2)*inchToMeter
 
 # Load data.
@@ -70,74 +69,37 @@ panelSize= 60*inchToMeter
 #########################################################
 # Mesh generation.
 
-## Truss E
+## Truss F
 lowerChordAxisE= geom.Segment3d(geom.Pos3d(0.0,0.0,0.0),geom.Pos3d(9.4707-0.8,0.0,0.0))
 upperChordAxisE= geom.Segment3d(geom.Pos3d(0.0,0.0,depth),geom.Pos3d(9.4707-0.8,0.0,depth))
-trussE= truss_generators.FanTruss(lowerChordAxisE, upperChordAxisE, trussModule= panelSize)
-trussE.name= '2E'
-trussE.lowerChordMaterial= lowerChordSection
-trussE.upperChordMaterial= upperChordSection
-trussE.diagonalMaterial= diagonalsMaterial
-trussE.diagonalArea= diagonalSectionsGeometry.A()
-trussE.postsMaterial= postsSection
+trussF= truss_generators.FanTruss(lowerChordAxisE, upperChordAxisE, trussModule= panelSize)
+trussF.name= '2F'
+trussF.lowerChordMaterial= lowerChordSection
+trussF.upperChordMaterial= upperChordSection
+trussF.diagonalMaterial= diagonalsMaterial
+trussF.diagonalArea= diagonalSectionsGeometry.A()
+trussF.postsMaterial= postsSection
 
-trussE.genMesh(feProblem)
-
-## Joist
-points= preprocessor.getMultiBlockTopology.getPoints
-trussEUpperChordFrontEnd= trussE.getUpperChordFrontEndPoint()
-joist_A= points.newPntFromPos3d(trussEUpperChordFrontEnd.getPos)
-joist_B= points.newPntFromPos3d(geom.Pos3d(11.3564-0.8,0.0,depth))
-joist_l= preprocessor.getMultiBlockTopology.getLines.newLine(joist_A.tag,joist_B.tag)
-joistTrf= trussE.crdTransf
-E= 2e6*6894.76 # Modulus of elasticity (Pa)
-lvl= typical_materials.MaterialData(name='LVL',E=E,nu=0.2,rho=500)
-joistSectionGeometry= sp.RectangularSection('joistSection',3.5*0.0254,6*0.0254)
-joistSection=  joistSectionGeometry.defElasticShearSection3d(preprocessor,lvl)
-seedElemHandler= preprocessor.getElementHandler.seedElemHandler
-seedElemHandler.defaultMaterial= joistSection.name
-seedElemHandler.defaultTransformation= joistTrf.name
-elem= seedElemHandler.newElement("ElasticBeam3d",xc.ID([0,0]))
-joist_l.genMesh(xc.meshDir.I)
-
-## Sheathing (in tension between trusses)
-points= preprocessor.getMultiBlockTopology.getPoints
-sheathing_A= trussE.getUpperChordFrontEndPoint()
-sheathing_B= joist_B
-sheathing_l= preprocessor.getMultiBlockTopology.getLines.newLine(sheathing_A.tag,sheathing_B.tag)
-sheathingTrf= trussE.crdTransf
-E= 2e6*6894.76 # Modulus of elasticity (Pa)
-sheathingMat= typical_materials.MaterialData(name='sheathing',E=E,nu=0.2,rho=500)
-sheathingSectionGeometry= sp.RectangularSection('sheathingSection',0.75*inchToMeter,centerSpacingTrusses)
-sheathingSection=  sheathingSectionGeometry.defElasticShearSection3d(preprocessor,sheathingMat)
-seedElemHandler= preprocessor.getElementHandler.seedElemHandler
-seedElemHandler.defaultMaterial= sheathingSection.name
-seedElemHandler.defaultTransformation= sheathingTrf.name
-elem= seedElemHandler.newElement("ElasticBeam3d",xc.ID([0,0]))
-sheathing_l.genMesh(xc.meshDir.I)
+trussF.genMesh(feProblem)
 
 #########################################################
 # Boundary conditions.
 
-## Truss E
-trussE.fixNodes(modelSpace)
+## Truss F
+trussF.fixNodes(modelSpace)
 
-## Joist
-modelSpace.constraints.newEqualDOF(trussEUpperChordFrontEnd.getNode().tag,joist_A.getNode().tag,xc.ID([0,1,2,3]))
-modelSpace.fixNode('000_FFF',joist_B.getNode().tag)
 
 # Sets
 
 ## Upper chord
 upperChordSet= preprocessor.getSets.defSet("upperChordSet")
-for l in trussE.upperChordSet.getLines:
+for l in trussF.upperChordSet.getLines:
     upperChordSet.getLines.append(l)
-upperChordSet.getLines.append(joist_l)
 upperChordSet.fillDownwards()
 
 ## Lower chord
 lowerChordSet= preprocessor.getSets.defSet("lowerChordSet")
-for l in trussE.lowerChordSet.getLines:
+for l in trussF.lowerChordSet.getLines:
     lowerChordSet.getLines.append(l)
 lowerChordSet.fillDownwards()
 
@@ -154,40 +116,31 @@ loadCaseManager.defineSimpleLoadCases(loadCaseNames)
 ## Self weight.
 selfWeight= loadCaseManager.setCurrentLoadCase('selfWeight')
 gravity= xc.Vector([0.0,0.0,9.81]) #Acceleration of gravity (m/s2)
-trussE.createSelfWeightLoads(grav= gravity)
-joist_l.createInertiaLoads(gravity)
+trussF.createSelfWeightLoads(grav= gravity)
 
 ## Dead load.
 roofDeadLoad= xc.Vector([0.0,0.0,-1.34e3*centerSpacingTrusses])
 cLC= loadCaseManager.setCurrentLoadCase('deadLoad')
-for e in trussE.upperChordSet.elements:
+for e in trussF.upperChordSet.elements:
   e.vector3dUniformLoadGlobal(roofDeadLoad)
-for e in joist_l.elements:
-  e.vector3dUniformLoadGlobal(joistLoadFactor*roofDeadLoad)
 
 ## Live load.
 roofLiveLoad= xc.Vector([0.0,0.0,-1.92e3*centerSpacingTrusses])
 cLC= loadCaseManager.setCurrentLoadCase('liveLoad')
-for e in trussE.upperChordSet.elements:
+for e in trussF.upperChordSet.elements:
   e.vector3dUniformLoadGlobal(roofLiveLoad)
-for e in joist_l.elements:
-  e.vector3dUniformLoadGlobal(joistLoadFactor*roofLiveLoad)
 
 ## Snow load.
 roofSnowLoad= xc.Vector([0.0,0.0,0.0*centerSpacingTrusses])
 cLC= loadCaseManager.setCurrentLoadCase('snowLoad')
-# for e in trussE.upperChordSet.elements:
+# for e in trussF.upperChordSet.elements:
 #   e.vector3dUniformLoadGlobal(roofSnowLoad)
-# for e in joist_l.elements:
-#   e.vector3dUniformLoadGlobal(joistLoadFactor*roofSnowLoad)
 
 ## Wind load.
 roofWindLoad= xc.Vector([0.0,0.0,0.0*centerSpacingTrusses])
 cLC= loadCaseManager.setCurrentLoadCase('windLoad')
-# for e in trussE.upperChordSet.elements:
+# for e in trussF.upperChordSet.elements:
 #   e.vector3dUniformLoadGlobal(roofWindLoad)
-# for e in joist_l.elements:
-#   e.vector3dUniformLoadGlobal(joistLoadFactor*roofWindLoad)
 
 #Load combinations
 combContainer= combs.CombContainer()
@@ -217,7 +170,12 @@ combContainer.SLS.qp.add('LIVE', '1.0*liveLoad')
 xcTotalSet= preprocessor.getSets.getSet("total")
 
 def reportResults(combName):
-    writeResults(combName,'2nd floor',[trussE])
+    writeResults(combName,'2nd floor',[trussF])
 
+#########################################################
+# Graphic stuff.
+modelSpace.cameraParameters= vtk_graphic_base.CameraParameters('Custom')
+modelSpace.cameraParameters.viewUpVc= [0,0,1]
+modelSpace.cameraParameters.posCVc=  [0,-100,0]
     
 
