57719716628140626d4a791db8f3e77ec3a5143f
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-01-21 19:00:27 +0100

    Design of OSB panel for floor sheathing

--- OXapp/wood_structure/sheathing/burned_xc_model.py
+++ OXapp/wood_structure/sheathing/burned_xc_model.py
@@ -23,12 +23,9 @@ modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
 
 # Materials
 # Mechanical properties taken from:
-# http://www.pfsteco.com/techtips/pdf/tt_plywooddesigncapacities
-structuralPanelGeom= AWCNDS_materials.PlywoodPanels['19/32']#['3/8']#
-plywood= typical_materials.MaterialData(name='Douglas-Fri Plywood',E=4.2e9,nu=0.2,rho=500)
-section= structuralPanelGeom.defElasticShearSection2d(preprocessor,plywood)
-
-thickness= structuralPanelGeom.h
+structuralPanel= AWCNDS_materials.OSBPanels['19/32']
+section= structuralPanel.defElasticShearSection2d(preprocessor, angle= 0.0)
+thickness= structuralPanel.h
 
 spanBendingStiffness= (32-1.5+0.25)*0.0254
 spanInternalForces= 32*0.0254
@@ -38,18 +35,11 @@ pt1= pointHandler.newPntFromPos3d(geom.Pos3d(0.0,0.0,0.0))
 pt2= pointHandler.newPntFromPos3d(geom.Pos3d(span,0.0,0.0))
 pt3= pointHandler.newPntFromPos3d(geom.Pos3d(2.0*span,0.0,0.0))
 pt4= pointHandler.newPntFromPos3d(geom.Pos3d(3.0*span,0.0,0.0))
-pt11= pointHandler.newPntFromPos3d(geom.Pos3d(0.0,thickness,0.0))
-pt12= pointHandler.newPntFromPos3d(geom.Pos3d(span,thickness,0.0))
-pt13= pointHandler.newPntFromPos3d(geom.Pos3d(2.0*span,thickness,0.0))
-pt14= pointHandler.newPntFromPos3d(geom.Pos3d(3.0*span,thickness,0.0))
 
 lines= preprocessor.getMultiBlockTopology.getLines
 l1= lines.newLine(pt1.tag,pt2.tag)
 l2= lines.newLine(pt2.tag,pt3.tag)
 l3= lines.newLine(pt3.tag,pt4.tag)
-l11= lines.newLine(pt11.tag,pt12.tag)
-l12= lines.newLine(pt12.tag,pt13.tag)
-l13= lines.newLine(pt13.tag,pt14.tag)
 
 supSet= preprocessor.getSets.defSet("sup")
 supSet.getLines.append(l1)
@@ -109,35 +99,37 @@ preprocessor.getLoadHandler.addToDomain("totalLoad")
 analisis= predefined_solutions.simple_static_linear(sheathingBeam)
 result= analisis.analyze(1)
 
-# Bending and shear strength (5-ply)
+# Bending and shear strength
 CD= AWCNDS_materials.getLoadDurationFactor(0.5/365.25/24)
 print("Cd= ",CD)
-Ft= 3640*4.44822/0.3048/section.sectionProperties.A
-Fb= CD*444.0/structuralPanelGeom.Wzel()*4.44822*0.0254/0.3048
-Fv= CD*215*4.44822/0.3048/structuralPanelGeom.h
+Fb= CD*structuralPanel.getFb(angle= 0.0)
+Fv= CD*structuralPanel.getFv()
+FbS= Fb/CD*structuralPanel.Wzel()/(4.44822*0.0254/0.3048)
 
 sgMax= -1e6
 tauMax= -1e6
 for e in supSet.elements:
     e.getResistingForce()
     m1= e.getM1
-    sg1= abs(m1/section.sectionProperties.I*structuralPanelGeom.h/2)
+    sg1= abs(m1/section.sectionProperties.I*structuralPanel.h/2)
     tau1= abs(e.getV1/section.sectionProperties.A)
     sgMax= max(sgMax,sg1)
     tauMax= max(tauMax,tau1)
     m2= e.getM2
-    sg2= abs(m2/section.sectionProperties.I*structuralPanelGeom.h/2)
+    sg2= abs(m2/section.sectionProperties.I*structuralPanel.h/2)
     tau2= abs(e.getV2/section.sectionProperties.A)
     sgMax= max(sgMax,sg2)
     tauMax= max(tauMax,tau2)
 
+print('span= ', span, ' m (', span/0.0254, 'in)')
+print('thickness= ', thickness*1e3, ' mm')
 print('sgMax= ', sgMax/1e6,' MPa')
 print('Fb= ', Fb/1e6,' MPa')
+print('FbS= ', FbS,' (lb-in/ft of width)')
 if(Fb>sgMax):
     print('OK')
 else:
     print('KO')
-#print('Ft= ', Ft/1e6,' MPa')
 print('Fv= ', Fv/1e6,' MPa')
 print('tauMax= ', tauMax/1e6,' MPa')
 if(Fv>tauMax):
