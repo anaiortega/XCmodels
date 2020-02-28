288b78cd1e2532c400875e0d87de681b557198f0
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-02-28 16:31:29 +0100

    Minor debugging

--- OXapp/wood_structure/headers/facade_headers/short_facade_headers.py
+++ OXapp/wood_structure/headers/facade_headers/short_facade_headers.py
@@ -10,6 +10,7 @@ from solution import predefined_solutions
 from materials.sections import section_properties
 from materials import typical_materials
 from materials.awc_nds import AWCNDS_materials
+from materials.awc_nds import structural_panels
 
 
 # Loads
@@ -29,7 +30,7 @@ nodes= preprocessor.getNodeHandler
 modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
 
 # Materials LSL 1.55E (page 4 of the PDF document from "SolidStart")
-header= AWCNDS_materials.LSL155Headers['3.5x7-1/4']
+header= structural_panels.LSL155Headers['3.5x7-1/4']
 section= header.defElasticShearSection2d(preprocessor)
 
 # Header geometry
@@ -46,7 +47,6 @@ l1= lineHandler.newLine(p0.tag,p1.tag)
 
 # Mesh
 modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
-nodes.newSeedNode()
 trfs= preprocessor.getTransfCooHandler
 lin= trfs.newLinearCrdTransf2d("lin")
 seedElemHandler= preprocessor.getElementHandler.seedElemHandler
