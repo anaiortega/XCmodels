6cb80bd72218da2518b778172d22d0a2751fd2cc
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-04-08 17:12:30 +0200

    Update syntax

--- OXapp/wood_structure/stair_connections/D_frontal_ledger_to_studs_connection/frontal_ledger_connection_check.py
+++ OXapp/wood_structure/stair_connections/D_frontal_ledger_to_studs_connection/frontal_ledger_connection_check.py
@@ -3,6 +3,7 @@
 from __future__ import division
 from __future__ import print_function
 
+import math
 
 inch2meter= 0.0254
 pound2Newton=  4.4482216282509
@@ -14,13 +15,29 @@ studSpacing= 12*inch2meter
 landingWidth= 1.9 # meters
 deadLoad= 1197*studSpacing*landingWidth/2.0 # Dead load.
 liveLoad= 4788*studSpacing*landingWidth/2.0 # Live load.
-maxLoad= deadLoad+liveLoad # Bearing load.
+maxShearLoad= deadLoad+liveLoad # Bearing load.
 
-connectionAllowableLoad= 555*pound2Newton
-capacityFactor= maxLoad/connectionAllowableLoad
+connectionAllowableShearLoad= 555*pound2Newton
+shearCapacityFactor= maxShearLoad/connectionAllowableShearLoad
 
-print('Max. load: ',maxLoad/1e3,'kN (', maxLoad/pound2Newton,'pounds)')
-print('Connection allowable load: ',connectionAllowableLoad/1e3,'kN (', connectionAllowableLoad/pound2Newton,'pounds)')
-print('Connection capacity factor: ',capacityFactor)
+print('Max. load: ',maxShearLoad/1e3,'kN (', maxShearLoad/pound2Newton,'pounds)')
+print('Connection allowable shear load: ',connectionAllowableShearLoad/1e3,'kN (', connectionAllowableShearLoad/pound2Newton,'pounds)')
+print('Connection shear capacity factor: ',shearCapacityFactor)
 
+# Bracing force estimation .01*studLoad according to the
+# publication Bracing for Stability
+# https://www.aisc.org/globalassets/aisc/research-library/bracing-for-stability.pdf
+numberOfScrews= 3
+studLoad= 10e3 # N
+threadPenetration= (6.0-2.0)*inch2meter
+connectionAllowableWithdrawalLoad= numberOfScrews*min(187*pound2Newton*threadPenetration/inch2meter,495*pound2Newton)
+ratio= connectionAllowableWithdrawalLoad/studLoad
+bracingCapacityFactor= .01/ratio
+
+capacityFactor= math.sqrt(shearCapacityFactor**2+bracingCapacityFactor**2)
+
+
+print('Connection allowable withdrawal load: ',connectionAllowableWithdrawalLoad/1e3,'kN (', connectionAllowableWithdrawalLoad/pound2Newton,'pounds)')
+print('ratio: ',ratio*100,'%')
+print('Connection bracing capacity factor: ',bracingCapacityFactor)
 
