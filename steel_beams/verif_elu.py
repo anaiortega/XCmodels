21bdfb52cab1816d545a07776e54fcf5488c9b85
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-05-02 19:44:17 +0200

    Update code

--- steel_beams/verif_elu.py
+++ steel_beams/verif_elu.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from postprocess import output_handler
 
 execfile('model_data.py')
-lsd.LimitStateData.envConfig= cfg
+#lsd.LimitStateData.envConfig= cfg
 from materials.ec3 import EC3Beam as ec3b
 
 crossSectionClass= 1
 ec3beams= list()
 for key in lineDict:
-  ec3beams.append(ec3b.EC3Beam(lineDict[key],IPE450A))
+    l= lineDict[key]
+    ec3beams.append(ec3b.EC3Beam(l.name,IPE450A, lstLines= [l]))
 
 def resultComb(prb,nmbComb):
   preprocessor.resetLoadCase()
@@ -18,7 +20,7 @@ def resultComb(prb,nmbComb):
   analysis= solution.simpleStaticLinear(prb)
   result= analysis.analyze(1)
   for l in ec3beams:
-    l.updateLateralBucklingReductionFactor(crossSectionClass)
+    l.updateLateralBucklingReductionFactor()
   result= analysis.analyze(1) #Update resistant values 
   preprocessor.getLoadHandler.removeFromDomain(nmbComb)
 
@@ -36,39 +38,9 @@ resultComb(mainBeam,"ELU01")
 #resultComb(mainBeam,"ELU02")
 #resultComb(mainBeam,"ELU03")
 
+#########################################################
+# Graphic stuff.
+oh= output_handler.OutputHandler(modelSpace)
 
-import vtk
-from postprocess.xcVtk import vtk_graphic_base
-from postprocess.xcVtk.FE_model import vtk_FE_graphic
-from postprocess.xcVtk import vtk_internal_force_diagram as ifd
-from postprocess.xcVtk import element_property_diagram as epd
-
-defGrid= vtk_graphic_base.RecordDefGrid()
-defGrid.nmbSet= "total"
-
-#diagram= ifd.InternalForceDiagram(0.1,1e-3,[setMainBeam],"Mz")
-diagram= epd.ElementPropertyDiagram(1,1,[setMainBeam],"chiLT")
-#diagram= epd.ElementPropertyDiagram(scaleFactor=10,fUnitConv=1,sets=[setMainBeam],propertyName="FCTNCP")
-#diagram= epd.ElementPropertyDiagram(-0.02,1e-3,[setMainBeam],"Mz-")
-#diagram= epd.ElementPropertyDiagram(-0.02,1e-3,[setMainBeam],"Mz+")
-#diagram= epd.ElementPropertyDiagram(10,1,[setMainBeam],"FCVCP")
-#diagram= epd.ElementPropertyDiagram(0.1,1e-3,[setMainBeam],"Vy+")
-#diagram= epd.ElementPropertyDiagram(0.1,1e-3,[setMainBeam],"Vy-")
-diagram.addDiagram()
-
-defDisplay= vtk_FE_graphic.RecordDefDisplayEF()
-defDisplay.cameraParameters= vtk_graphic_base.CameraParameters('YPos')
-defDisplay.setupGrid(preprocessor.getSets.getSet('total'))
-defDisplay.defineMeshScene(None)
-defDisplay.appendDiagram(diagram) #Append diagram to the scene.
-
-#execfile('draw_supports.py')
-#defDisplay.renderer.AddActor(supportsActor)
-
-defDisplay.displayScene()
-
-#ec5CS.printResultsELU(elems,sccPanne)
-
-
-
+oh.displayElementValueDiagram('chiLT', setToDisplay= setMainBeam)
 
