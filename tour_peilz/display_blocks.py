d16ec5e685df3085e77743c750e380db80e9442c
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-07-01 13:47:37 +0200

    Rampes La Tour de Peilz moved to its own project repository

--- tour_peilz/display_blocks.py
+++
@@ -1,6 +0,0 @@
-execfile('./xc_model_data.py')
-from postprocess.xcVtk.CAD_model import vtk_CAD_graphic
-
-displaySettings= vtk_CAD_graphic.DisplaySettingsBlockTopo()
-setDisp= roof_set+parapets_set # bulkheads_set #sides_set #floor_set
-displaySettings.displayBlocks(setDisp,caption= setDisp.name+' set')
