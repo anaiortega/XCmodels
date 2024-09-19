337dce7326da75f50720ff6ab77f24861a5dfa26
Ana <ana.ortega.ort@gmail.com>
2024-09-19 10:54:27 +0200

    delete old models

--- workingModel/display/display_blocks.py
+++
@@ -1,9 +0,0 @@
-
-
-exec(open('../model_gen.py').read()) #FE model generation
-
-from postprocess.xcVtk.CAD_model import vtk_CAD_graphic
-displaySettings= vtk_CAD_graphic.DisplaySettingsBlockTopo()
-displaySettings.displayBlocks(setToDisplay= overallSet,caption= 'Model grid')
-
-
--- workingModel/env_config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Default configuration of environment variables.
-cfg=default_config.EnvConfig(language='sp', resultsPath= 'results/', intForcPath= 'internalForces/',verifPath= 'verifications/')
-
